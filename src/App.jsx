782c3eb3156d765370f3a0ff6d1558748287f114
TKC <2300033901@kluniversity.in>
2025-08-04 00:06:05 +0530

    modified 3 time

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ function App() {
     <div>
       <h3>Hello Sanatana Dharama Laxman</h3>
       <h1>This is Chandan </h1>
+      <h1>Testing the gh pages</h1>
+      <h1>Hello KLU</h1>
     </div>
   )
 }
